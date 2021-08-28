2ef965f78093fafcc7c598f32ee825ede305a24c
Kenya Moore <kenyacmoore@gmail.com>
2021-08-27 21:21:22 -0400

    ran ng new and made a photo gallery component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoGalleryComponent
   ],
   imports: [
     BrowserModule,
